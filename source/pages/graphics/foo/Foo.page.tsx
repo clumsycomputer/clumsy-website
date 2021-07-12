67f61295844518b0408c7c35d7cb82c1924d9fa4
jmath <clumsycomputer@gmail.com>
2021-07-12 01:26:17 -0600

    feature(simple-graphics)): smeagle beagle

--- source/pages/graphics/foo/Foo.page.tsx
+++ source/pages/graphics/foo/Foo.page.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { Circle, getRotatedLoopPoints, RotatedLoop } from './circleStuff'
+import {
+  getCommonalityWave,
+  getElementIndices,
+  getFilteredRhythm,
+  getNaturalRhythm,
+} from './rhythmStuff'
 
 export default {
   pageRoute: '/graphics/foo',
@@ -11,48 +17,34 @@ export default {
 }
 
 function Foo() {
+  const waveA = getCommonalityWave({
+    baseRhythm: getFilteredRhythm({
+      rhythmSequence: [
+        getNaturalRhythm({
+          rhythmResolution: 12,
+          rhythmDensity: 11,
+          rhythmPhase: 0,
+        }),
+        getNaturalRhythm({
+          rhythmResolution: 11,
+          rhythmDensity: 7,
+          rhythmPhase: 0,
+        }),
+        getNaturalRhythm({
+          rhythmResolution: 7,
+          rhythmDensity: 5,
+          rhythmPhase: 0,
+        }),
+      ],
+    }),
+  })
   const baseCircleA: Circle = {
-    radius: 25,
+    radius: 45,
     center: {
       x: 50,
       y: 50,
     },
   }
-  const rotatedLoopA: RotatedLoop = {
-    baseLoop: {
-      baseCircle: baseCircleA,
-      childCircle: {
-        relativeRadius: 0.675,
-        relativeDepth: 0.875,
-        phaseAngle: (2 * Math.PI) / 5,
-      },
-    },
-    rotationAnchor: 'child',
-    rotationAngle: (2 * Math.PI) / 3,
-  }
-  const rotatedLoopB: RotatedLoop = {
-    baseLoop: {
-      baseCircle: {
-        ...baseCircleA,
-        radius: 23,
-      },
-      childCircle: {
-        relativeRadius: 0.675,
-        relativeDepth: 0.875,
-        phaseAngle: (2 * Math.PI) / 5,
-      },
-    },
-    rotationAnchor: 'child',
-    rotationAngle: (2 * Math.PI) / 7,
-  }
-  const loopPointsA = getRotatedLoopPoints({
-    sampleCount: 256,
-    someRotatedLoop: rotatedLoopA,
-  })
-  const loopPointsB = getRotatedLoopPoints({
-    sampleCount: 256,
-    someRotatedLoop: rotatedLoopB,
-  })
   return (
     <svg
       style={{
@@ -65,22 +57,70 @@ function Foo() {
       viewBox={'-10 -10 120 120'}
       imageRendering={'optimizeQuality'}
     >
-      <polygon
-        points={loopPointsA
-          .map((someLoopPoint) => `${someLoopPoint.x},${someLoopPoint.y}`)
-          .join(' ')}
-        stroke={'black'}
-        strokeWidth={0.25}
-        fill={'none'}
-      />
-      <polygon
-        points={loopPointsB
-          .map((someLoopPoint) => `${someLoopPoint.x},${someLoopPoint.y}`)
-          .join(' ')}
-        stroke={'black'}
-        strokeWidth={0.25}
-        fill={'none'}
-      />
+      {waveA.map((cellValue, cellIndex) => {
+        const cellAngle =
+          ((2 * Math.PI) / waveA.length) * cellIndex + Math.PI / 2
+        const polyBaseCenter = {
+          x:
+            (baseCircleA.radius / cellValue) * Math.cos(cellAngle) +
+            baseCircleA.center.x,
+          y:
+            (baseCircleA.radius / cellValue) * Math.sin(cellAngle) +
+            baseCircleA.center.y,
+        }
+        const relativeAngleFromCenter =
+          Math.atan2(
+            polyBaseCenter.y - baseCircleA.center.y,
+            polyBaseCenter.x - baseCircleA.center.x
+          ) -
+          Math.PI / 2
+        const phaseAdjuster =
+          polyBaseCenter.y < 50 ? Math.PI : Math.PI * (cellIndex % 2)
+        return (
+          <Fragment>
+            {getElementIndices({
+              someSpace: getNaturalRhythm({
+                rhythmResolution: 2 * cellValue,
+                rhythmDensity: cellValue + 1,
+                rhythmPhase: 0,
+              }),
+              targetValue: true,
+            }).map((nestIndex, indexIndex) => (
+              <polygon
+                points={getRotatedLoopPoints({
+                  sampleCount: 256,
+                  someRotatedLoop: {
+                    baseLoop: {
+                      baseCircle: {
+                        center: polyBaseCenter,
+                        radius:
+                          indexIndex !== 4
+                            ? 2 * cellValue - nestIndex / 1.125 - cellValue / 3
+                            : 0,
+                      },
+                      childCircle: {
+                        relativeRadius: 0.9 / ((nestIndex + 1) / 2),
+                        relativeDepth: 0.5 / ((nestIndex + 1) / 2),
+                        phaseAngle: relativeAngleFromCenter - phaseAdjuster,
+                      },
+                    },
+                    rotationAnchor: 'base',
+                    rotationAngle:
+                      Math.PI / 2 -
+                      relativeAngleFromCenter -
+                      (polyBaseCenter.y < 35 ? Math.PI : 0),
+                  },
+                })
+                  .map((somePoint) => `${somePoint.x},${somePoint.y}`)
+                  .join(' ')}
+                stroke={'black'}
+                strokeWidth={0.25}
+                fill={'none'}
+              />
+            ))}
+          </Fragment>
+        )
+      })}
     </svg>
   )
 }

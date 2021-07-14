e6e1ee5c9cc6ef09568ba93c766b68eb908f1bc6
jmath <clumsycomputer@gmail.com>
2021-07-14 14:44:19 -0600

    chore(simple-graphics): setup bar graphic page

---
+++ source/pages/graphics/bar/Bar.page.tsx
@@ -0,0 +1,12 @@
+export default {
+  pageRoute: '/graphics/bar',
+  PageContent: Bar,
+  htmlTitle: 'bar - jmath',
+  htmlDescription: 'a bar graphic',
+  generatePdf: false,
+  pdfFileName: 'bar',
+}
+
+function Bar() {
+  return 'hi'
+}

66bd3209d74f834f5e6a92fe4fd9d5d886554454
jmath <clumsycomputer@gmail.com>
2021-07-25 21:31:32 -0600

    chore(graphics/waldo): new stroke

--- source/pages/graphics/waldo/Waldo.page.tsx
+++ source/pages/graphics/waldo/Waldo.page.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
-import { Point } from '../../../library/circleStuff'
+import React, { Fragment } from 'react'
+import {
+  Circle,
+  getMirroredPoint,
+  getRotatedLoopPoint,
+  GetRotatedLoopPointApi,
+  Point,
+  RotatedLoop,
+} from '../../../library/circleStuff'
+import { reduceRhythmSequence } from '../../../library/helperStuff'
+import { getNaturalCompositeRhythm } from '../../../library/rhythmStuff'
 
 export default {
   pageRoute: '/graphics/waldo',
@@ -10,8 +19,14 @@ export default {
   pdfFileName: 'waldo',
 }
 
+const globalSampleCount = 512
+
 function Waldo() {
-  // const baseLineA =
+  const rootCircle: Circle = {
+    radius: 50,
+    center: { x: 50, y: 50 },
+  }
+  const cellLength = 1 / 2 / 2
   return (
     <svg
       style={{
@@ -25,90 +40,142 @@ function Waldo() {
       imageRendering={'optimizeQuality'}
     >
       <rect x={-10} y={-10} width={120} height={120} fill={'black'} />
+      {getOscillatedLoopPoints({
+        sampleCount: globalSampleCount,
+        someRotatedLoop: {
+          baseCircle: {
+            ...rootCircle,
+            radius: 12,
+          },
+          childCircle: {
+            relativeRadius: 7 / 12,
+            relativeDepth: 1,
+            phaseAngle: 2 * Math.PI * (5 / 12),
+          },
+          rotationAnchor: 'base',
+          rotationAngle: 2 * Math.PI * (5 / 12),
+        },
+        oscillationRadius: 0.4,
+        oscillationFrequency: getWaveFrequency({
+          originNote: 211,
+          scaleResolution: 18,
+          noteIndex: -2,
+        }),
+      }).map((somePoint) => {
+        const mirroredPoint = getMirroredPoint({
+          mirrorAngle: Math.PI / 2,
+          basePoint: somePoint,
+          originPoint: rootCircle.center,
+        })
+        return (
+          <Fragment>
+            <rect
+              fill={'#ff2e00'}
+              x={somePoint.x - cellLength / 2}
+              y={somePoint.y - cellLength / 2}
+              width={cellLength}
+              height={cellLength}
+            />
+            <rect
+              fill={'#ff2e00'}
+              x={mirroredPoint.x - cellLength / 2}
+              y={mirroredPoint.y - cellLength / 2}
+              width={cellLength}
+              height={cellLength}
+            />
+          </Fragment>
+        )
+      })}
+      {getOscillatedLoopPoints({
+        sampleCount: globalSampleCount,
+        someRotatedLoop: {
+          baseCircle: {
+            ...rootCircle,
+            radius: 12,
+          },
+          childCircle: {
+            relativeRadius: 7 / 12,
+            relativeDepth: 1,
+            phaseAngle: 2 * Math.PI * (5 / 12),
+          },
+          rotationAnchor: 'base',
+          rotationAngle: 2 * Math.PI * (5 / 12),
+        },
+        oscillationRadius: 0.4,
+        oscillationFrequency: getWaveFrequency({
+          originNote: 211,
+          scaleResolution: 18,
+          noteIndex: -0,
+        }),
+      }).map((somePoint) => {
+        const mirroredPoint = getMirroredPoint({
+          mirrorAngle: Math.PI / 2,
+          basePoint: somePoint,
+          originPoint: rootCircle.center,
+        })
+        return (
+          <Fragment>
+            <rect
+              fill={'black'}
+              x={somePoint.x - cellLength / 2}
+              y={somePoint.y - cellLength / 2}
+              width={cellLength}
+              height={cellLength}
+            />
+            <rect
+              fill={'black'}
+              x={mirroredPoint.x - cellLength / 2}
+              y={mirroredPoint.y - cellLength / 2}
+              width={cellLength}
+              height={cellLength}
+            />
+          </Fragment>
+        )
+      })}
     </svg>
   )
 }
 
-interface PolarChainFunction {
-  functionChain: Array<TangentialPolarFunction | OrthogonalPolarFunction>
-}
-interface GetTangentialFunctionPointsApi {
+interface GetOscillatedLoopPointsApi
+  extends Pick<GetRotatedLoopPointApi, 'someRotatedLoop'> {
   sampleCount: number
-  someTangentialFunction: TangentialPolarFunction
+  oscillationRadius: number
+  oscillationFrequency: number
 }
 
-function getTangentialFunctionPoints(
-  api: GetTangentialFunctionPointsApi
-): Array<Point> {
-  const { sampleCount, someTangentialFunction } = api
-  const { originPoint, getPointRadius, angleRange } = someTangentialFunction
-  const angleRangeDelta = angleRange[1] - angleRange[0]
-  return new Array(sampleCount).fill(undefined).map((_, sampleIndex) =>
-    getTangentialPoint({
-      originPoint,
-      getPointRadius,
-      pointAngle: (angleRangeDelta / sampleCount) * sampleIndex + angleRange[0],
+function getOscillatedLoopPoints(api: GetOscillatedLoopPointsApi) {
+  const {
+    sampleCount,
+    someRotatedLoop,
+    oscillationRadius,
+    oscillationFrequency,
+  } = api
+  return new Array(sampleCount).fill(undefined).map<Point>((_, sampleIndex) => {
+    const currentSampleAngle = ((2 * Math.PI) / sampleCount) * sampleIndex
+    const basePoint = getRotatedLoopPoint({
+      someRotatedLoop,
+      sampleAngle: currentSampleAngle,
     })
-  )
+    return {
+      x:
+        oscillationRadius *
+          Math.cos(oscillationFrequency * currentSampleAngle) +
+        basePoint.x,
+      y:
+        oscillationRadius *
+          Math.sin(oscillationFrequency * currentSampleAngle) +
+        basePoint.y,
+    }
+  })
 }
 
-interface GetTangentialPointApi
-  extends Pick<TangentialPolarFunction, 'originPoint' | 'getPointRadius'> {
-  pointAngle: Parameters<TangentialPolarFunction['getPointRadius']>[0]
+interface GetWaveFrequencyApi {
+  originNote: number
+  scaleResolution: number
+  noteIndex: number
 }
 
-function getTangentialPoint(api: GetTangentialPointApi): Point {
-  const { getPointRadius, originPoint, pointAngle } = api
-  const pointRadius = getPointRadius(pointAngle)
-  return {
-    x: pointRadius * Math.cos(pointAngle) + originPoint.x,
-    y: pointRadius * Math.sin(pointAngle) + originPoint.y,
-  }
-}
-
-interface TangentialPolarFunction {
-  angleRange: [number, number]
-  getPointRadius: (pointAngle: number) => number
-  originPoint: Point
-}
-
-interface GetOrthogonalFunctionPointsApi {
-  sampleCount: number
-  someOrthogonalFunction: OrthogonalPolarFunction
-}
-
-function getOrthogonalFunctionPoints(
-  api: GetOrthogonalFunctionPointsApi
-): Array<Point> {
-  const { sampleCount, someOrthogonalFunction } = api
-  const { originPoint, getPointAngle, radiusRange } = someOrthogonalFunction
-  const radiusRangeDelta = radiusRange[1] - radiusRange[0]
-  return new Array(sampleCount).fill(undefined).map((_, sampleIndex) =>
-    getOrthogonalPoint({
-      originPoint,
-      getPointAngle,
-      pointRadius:
-        (radiusRangeDelta / sampleCount) * sampleIndex + radiusRange[0],
-    })
-  )
-}
-
-interface GetOrthogonalPointApi
-  extends Pick<OrthogonalPolarFunction, 'originPoint' | 'getPointAngle'> {
-  pointRadius: Parameters<OrthogonalPolarFunction['getPointAngle']>[0]
-}
-
-function getOrthogonalPoint(api: GetOrthogonalPointApi): Point {
-  const { getPointAngle, pointRadius, originPoint } = api
-  const pointAngle = getPointAngle(pointRadius)
-  return {
-    x: pointRadius * Math.cos(pointAngle) + originPoint.x,
-    y: pointRadius * Math.sin(pointAngle) + originPoint.y,
-  }
-}
-
-interface OrthogonalPolarFunction {
-  radiusRange: [number, number]
-  getPointAngle: (pointRadius: number) => number
-  originPoint: Point
+function getWaveFrequency(api: GetWaveFrequencyApi) {
+  const { originNote, scaleResolution, noteIndex } = api
+  return originNote * Math.pow(2, noteIndex / scaleResolution)
 }

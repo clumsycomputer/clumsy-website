0233fc8777abc98674c5a81c89cd5cb915d502ae
jmath <clumsycomputer@gmail.com>
2021-07-29 14:28:47 -0600

    feature(graphics/fred): colormap help

--- source/pages/graphics/fred/Fred.page.tsx
+++ source/pages/graphics/fred/Fred.page.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
 import {
   Circle,
-  getRotatedLoopChildCircle,
-  getRotatedLoopPoints,
+  getTracePoint,
+  Point,
   RotatedLoop,
 } from '../../../library/circleStuff'
+import { getUpdatedData } from '../../../library/getUpdatedData'
 import {
-  FreakPolygon,
-  FreakPolygons,
-  Graphic,
-  GraphicRectangle,
-} from './helpers'
+  CompositeLoop,
+  getCompositeCenterPoint,
+  getCompositeLoopPoints,
+  getDistanceBetweenPoints,
+  reduceRhythmSequence,
+} from '../../../library/helperStuff'
+import {
+  DiscreteRhythm,
+  getNaturalCompositeRhythm,
+} from '../../../library/rhythmStuff'
+import { getWaveFrequency } from '../waldo/helpers'
+import { FreakPolygon, Graphic, GraphicRectangle } from './helpers'
+// @ts-ignore
+import getColorMap from 'colormap'
 
 export default {
   pageRoute: '/graphics/fred',
@@ -33,24 +43,14 @@ function Fred() {
     center: { x: 50, y: 50 },
   }
   const rotatedLoopA: RotatedLoop = {
-    baseCircle: rootCircle,
+    baseCircle: { ...rootCircle, radius: 23 },
     childCircle: {
-      relativeRadius: 7 / 12,
+      relativeRadius: 10 / 12,
       relativeDepth: 1,
-      phaseAngle: 0,
+      phaseAngle: Math.PI / 2 / 3,
     },
     rotationAnchor: 'base',
-    rotationAngle: Math.PI / 2,
-  }
-  const rotatedLoopB: RotatedLoop = {
-    baseCircle: rootCircle,
-    childCircle: {
-      relativeRadius: 1,
-      relativeDepth: 1,
-      phaseAngle: 0,
-    },
-    rotationAnchor: 'base',
-    rotationAngle: Math.PI / 2,
+    rotationAngle: Math.PI / 2 / 3,
   }
   return (
     <Graphic
@@ -64,58 +64,151 @@ function Fred() {
       canvasRectangle={canvasRectangle}
     >
       <rect fill={'black'} {...canvasRectangle} />
-      <FreakPolygons
-        fillColor={'white'}
-        targetRectangle={canvasRectangle}
-        polygonLayers={[
-          {
-            baseSquares: {
-              oscillationSampleCount: 2048,
-              oscillationAmplitude: 5,
-              oscillationFrequency: 220,
-              oscillationPhase: Math.PI / 2,
-              squareBaseLength: 0.5,
+      {getNestCompositeLoopsPoints({
+        shiftAngle: -rotatedLoopA.rotationAngle,
+        shiftScalar: 2,
+        sampleCount: 128,
+        baseLoop: [
+          rotatedLoopA,
+          rotatedLoopA,
+          getUpdatedData({
+            baseData: rotatedLoopA,
+            dataUpdates: {
+              'childCircle.phaseAngle': (foo: number) => -foo,
+              rotationAngle: (foo: number) => foo + Math.PI / 3,
+              'childCircle.relativeRadius': (foo: number) => foo / 1.25,
             },
-            overlaySquares: {
-              oscillationSampleCount: 2048,
-              oscillationAmplitude: 4,
-              oscillationFrequency: 220,
-              oscillationPhase: Math.PI / 2,
-              squareBaseLength: 0.5,
+          }),
+          getUpdatedData({
+            baseData: rotatedLoopA,
+            dataUpdates: {
+              'childCircle.phaseAngle': (foo: number) => -foo,
+              rotationAngle: (foo: number) => foo + Math.PI / 2,
+              'childCircle.relativeRadius': (foo: number) => foo / 1.125,
             },
-            polygonOrigin: getRotatedLoopChildCircle({
-              someRotatedLoop: rotatedLoopA,
-            }).center,
-            polygonPoints: getRotatedLoopPoints({
-              sampleCount: 4,
-              someRotatedLoop: rotatedLoopA,
-            }),
-          },
-          {
-            baseSquares: {
-              oscillationSampleCount: 2048,
-              oscillationAmplitude: 4,
-              oscillationFrequency: 220,
-              oscillationPhase: Math.PI / 2,
-              squareBaseLength: 0.5,
-            },
-            overlaySquares: {
-              oscillationSampleCount: 2048,
-              oscillationAmplitude: 3,
-              oscillationFrequency: 220,
-              oscillationPhase: Math.PI / 2 - Math.PI / 12,
-              squareBaseLength: 0.5,
-            },
-            polygonOrigin: getRotatedLoopChildCircle({
-              someRotatedLoop: rotatedLoopB,
-            }).center,
-            polygonPoints: getRotatedLoopPoints({
-              sampleCount: 3,
-              someRotatedLoop: rotatedLoopB,
-            }),
-          },
-        ]}
-      />
+          }),
+        ],
+        nestRhythm: getNaturalCompositeRhythm({
+          rhythmResolution: 24,
+          rhythmParts: [
+            { rhythmDensity: 23, rhythmPhase: 0 },
+            { rhythmDensity: 11, rhythmPhase: 0 },
+          ],
+          rhythmPhase: 0,
+        }),
+      }).map((someNestLayer, layerIndex) => {
+        const colorMap = getColorMap({
+          colormap: 'par',
+          nshades: someNestLayer.rhythmDensity,
+          format: 'hex',
+          alpha: 1,
+        })
+        const oscillationAmp = Math.log(
+          ((rotatedLoopA.baseCircle.radius / someNestLayer.rhythmResolution) *
+            (someNestLayer.rhythmResolution - someNestLayer.rhythmIndex)) /
+            2
+        )
+        const squareBaseLength = oscillationAmp / 17
+        const greyScale =
+          (someNestLayer.rhythmDensity - layerIndex) /
+          someNestLayer.rhythmDensity
+        const greyValue = 255 - 255 * greyScale
+        return (
+          <FreakPolygon
+            // fillColor={`rgb(${greyValue},${greyValue},${greyValue})`}
+            fillColor={colorMap[layerIndex]}
+            targetRectangle={canvasRectangle}
+            polygonOrigin={someNestLayer.loopCenter}
+            polygonPoints={someNestLayer.loopPoints}
+            baseSquares={{
+              squareBaseLength,
+              oscillationAmplitude: oscillationAmp,
+              oscillationSampleCount: 1024 * 5,
+              oscillationFrequency: getWaveFrequency({
+                baseFrequency: 211,
+                scaleResolution: 1,
+                frequencyIndex: 0,
+              }),
+              oscillationPhase: 0,
+            }}
+            overlaySquares={{
+              // squareBaseLength: (squareBaseLength / 5) * 2,
+              squareBaseLength: squareBaseLength,
+              oscillationAmplitude: oscillationAmp,
+              oscillationSampleCount: 1024 * 5,
+              oscillationFrequency: getWaveFrequency({
+                baseFrequency: 440,
+                scaleResolution: 1,
+                frequencyIndex: 0,
+              }),
+              oscillationPhase: 9,
+            }}
+          />
+        )
+      })}
     </Graphic>
   )
+}
+
+interface GetNestCompositeLoopsPointsApi {
+  baseLoop: CompositeLoop
+  sampleCount: number
+  shiftAngle: number
+  shiftScalar: number
+  nestRhythm: DiscreteRhythm
+}
+
+function getNestCompositeLoopsPoints(api: GetNestCompositeLoopsPointsApi) {
+  const { sampleCount, baseLoop, shiftAngle, nestRhythm, shiftScalar } = api
+  const cellLoopBasePoints = getCompositeLoopPoints({
+    sampleCount,
+    baseLoops: baseLoop,
+  })
+  const compositeCenter = getCompositeCenterPoint({
+    baseLoops: baseLoop,
+  })
+  const maxShiftPoint: Point = getTracePoint({
+    somePoints: cellLoopBasePoints,
+    traceAngle: shiftAngle,
+    originPoint: compositeCenter,
+  })
+  const maxShiftRadius = getDistanceBetweenPoints({
+    pointA: compositeCenter,
+    pointB: maxShiftPoint,
+  })
+  return reduceRhythmSequence<any>({
+    baseRhythm: nestRhythm,
+    getCellResult: (someNestStuff) => {
+      const childShiftRadius =
+        shiftScalar *
+        (maxShiftRadius / someNestStuff.rhythmResolution) *
+        someNestStuff.rhythmIndex
+      const currentCenter: Point = {
+        x: childShiftRadius * Math.cos(shiftAngle) + compositeCenter.x,
+        y: childShiftRadius * Math.sin(shiftAngle) + compositeCenter.y,
+      }
+      return {
+        ...someNestStuff,
+        loopCenter: currentCenter,
+        loopPoints: cellLoopBasePoints.map((someBasePoint, pointIndex) => {
+          const maxRadius = getDistanceBetweenPoints({
+            pointA: compositeCenter,
+            pointB: someBasePoint,
+          })
+          const currentBaseRadius =
+            Math.log(
+              maxRadius -
+                (maxRadius / someNestStuff.rhythmResolution) *
+                  someNestStuff.rhythmIndex
+            ) * 5
+          const currentAngle =
+            ((2 * Math.PI) / cellLoopBasePoints.length) * pointIndex
+          return {
+            x: currentBaseRadius * Math.cos(currentAngle) + currentCenter.x,
+            y: currentBaseRadius * Math.sin(currentAngle) + currentCenter.y,
+          }
+        }),
+      }
+    },
+  })
 }

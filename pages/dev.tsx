f8510ca4e4922031fb78ae5bbdacce9b884fb547
jmath <clumsycomputer@gmail.com>
2022-09-21 18:21:04 -0600

    feature(DevPage): basic loop diagram setup

--- pages/dev.tsx
+++ pages/dev.tsx
@@ -6,7 +6,7 @@ import Zod from "zod";
 export default () => {
   const baseCircleNode = getCircleNode({
     nodeId: 0,
-    nodeName: "base circle",
+    nodeName: "baseCircle",
     nodeEncoding: {
       radius: 1,
       center: [0, 0],
@@ -17,15 +17,15 @@ export default () => {
   });
   const subCircleNode = getRelativeCircleNode({
     nodeId: 1,
-    nodeName: "sub circle",
+    nodeName: "subCircle",
     nodeEncoding: {
       baseCircleNode: baseCircleNode,
       relativeRadius: 0.675,
-      relativeDepth: 0.5,
+      relativeDepth: 0.5, // 0 breaks when should works
       relativePhase: Math.PI / 3,
     },
     nodeAttributes: {
-      stroke: "deepPink",
+      stroke: "deeppink",
     },
   });
   const loopNode: LoopNode = getLoopNode({
@@ -39,20 +39,117 @@ export default () => {
       stroke: "yellow",
     },
   });
-  const basicLoopDiagram = [baseCircleNode, subCircleNode, loopNode];
+  const traceStamp = 0.75;
+  const traceIntersectionCircleNode = getCircleNode({
+    nodeId: 3,
+    nodeName: "traceCircle",
+    nodeEncoding: getIntersectionCircle({
+      ...getIntersectionRadiusData({
+        baseCircle: baseCircleNode.nodeGeometry,
+        subCircle: subCircleNode.nodeGeometry,
+        relativeSubCirclePhase: subCircleNode.nodeEncoding.relativePhase,
+      }),
+      subCircleCenter: subCircleNode.nodeGeometry.center,
+      relativeIntersectionRadius: Math.sin(Math.PI * traceStamp),
+    }),
+    nodeAttributes: {
+      stroke: "deepskyblue",
+    },
+  });
+  const traceIntersectionAngle = getNormalizedAngle({
+    someAngle:
+      Math.PI +
+      subCircleNode.nodeEncoding.relativePhase -
+      getIntersectionBaseAngle({
+        baseCircleRadius: baseCircleNode.nodeGeometry.radius,
+        intersectionCircleRadius:
+          traceIntersectionCircleNode.nodeGeometry.radius,
+        baseSubCenterDistance: getDistanceBetweenPoints({
+          pointA: baseCircleNode.nodeGeometry.center,
+          pointB: subCircleNode.nodeGeometry.center,
+        }),
+      }) *
+        (Math.sin(2 * Math.PI * traceStamp) >= 0 ? 1 : -1),
+  });
+  const traceBasePointNode = getPointNode({
+    nodeId: 4,
+    nodeName: "traceBasePoint",
+    nodeEncoding: getCirclePoint({
+      someCircle: traceIntersectionCircleNode.nodeGeometry,
+      pointAngle: traceIntersectionAngle,
+    }),
+    nodeAttributes: {
+      fill: "lime",
+    },
+  });
+  const traceSubPointNode = getPointNode({
+    nodeId: 6,
+    nodeName: "traceSubPoint",
+    nodeEncoding: getCirclePoint({
+      someCircle: subCircleNode.nodeGeometry,
+      pointAngle: traceIntersectionAngle,
+    }),
+    nodeAttributes: {
+      fill: "lime",
+    },
+  });
+  const traceLoopPointNode = getPointNode({
+    nodeId: 7,
+    nodeName: "traceLoopPoint",
+    nodeEncoding: [
+      traceBasePointNode.nodeGeometry[0],
+      traceSubPointNode.nodeGeometry[1],
+    ],
+    nodeAttributes: {
+      fill: "black",
+    },
+  });
+  const linearTraceLoopPointNode = getPointNode({
+    nodeId: 8,
+    nodeName: "linearTraceLoopPoint",
+    nodeEncoding: getLoopTracePoint({
+      baseCircle: baseCircleNode.nodeGeometry,
+      subCircle: subCircleNode.nodeGeometry,
+      loopPoints: loopNode.nodeGeometry,
+      traceAngle: traceIntersectionAngle,
+    }),
+    nodeAttributes: {
+      fill: "white",
+    },
+  });
+  const basicLoopDiagram = [
+    baseCircleNode,
+    subCircleNode,
+    traceIntersectionCircleNode,
+    loopNode,
+    traceBasePointNode,
+    traceSubPointNode,
+    traceLoopPointNode,
+    linearTraceLoopPointNode,
+  ];
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
       <div></div>
-      <svg viewBox={"-2 -2 4 4"} width={250} height={250}>
+      <svg viewBox={"-2 -2 4 4"} width={512} height={512}>
         <rect x={-2} y={-2} width={4} height={4} fill={"grey"} />
         {basicLoopDiagram.map((someGeometryNode) => {
-          if (
+          if (someGeometryNode.nodeType === "point") {
+            return (
+              <circle
+                {...someGeometryNode.nodeAttributes}
+                key={someGeometryNode.nodeId}
+                cx={someGeometryNode.nodeGeometry[0]}
+                cy={someGeometryNode.nodeGeometry[1]}
+              />
+            );
+          } else if (
             someGeometryNode.nodeType === "circle" ||
             someGeometryNode.nodeType === "relativeCircle"
           ) {
             return (
               <circle
                 {...someGeometryNode.nodeAttributes}
+                key={someGeometryNode.nodeId}
                 r={someGeometryNode.nodeGeometry.radius}
                 cx={someGeometryNode.nodeGeometry.center[0]}
                 cy={someGeometryNode.nodeGeometry.center[1]}
@@ -62,6 +159,7 @@ export default () => {
             return (
               <polygon
                 {...someGeometryNode.nodeAttributes}
+                key={someGeometryNode.nodeId}
                 points={someGeometryNode.nodeGeometry
                   .map(
                     (someLoopPoint) => `${someLoopPoint[0]},${someLoopPoint[1]}`
@@ -69,6 +167,8 @@ export default () => {
                   .join(" ")}
               />
             );
+          } else {
+            throw new Error("invalid path reached: geometry node render");
           }
         })}
       </svg>
@@ -83,6 +183,31 @@ type LoopPoint = [x: number, y: number, outputAngle: number];
 interface Circle {
   radius: number;
   center: Point;
+}
+
+interface PointNode extends GeometryNode<"point", Point, Point> {}
+
+interface GetPointNodeApi
+  extends Pick<
+    PointNode,
+    "nodeId" | "nodeName" | "nodeEncoding" | "nodeAttributes"
+  > {}
+
+function getPointNode(api: GetPointNodeApi): PointNode {
+  const { nodeId, nodeName, nodeEncoding, nodeAttributes } = api;
+  return {
+    nodeId,
+    nodeName,
+    nodeEncoding,
+    nodeType: "point",
+    nodeAttributes: {
+      r: 0.05,
+      ...nodeAttributes,
+    },
+    get nodeGeometry(): Point {
+      return this.nodeEncoding;
+    },
+  };
 }
 
 interface CircleNode extends GeometryNode<"circle", Circle, Circle> {}
@@ -147,9 +272,15 @@ function getRelativeCircleNode(
       const baseCircle = baseCircleNode.nodeGeometry;
       const subCircleRadius = relativeRadius * baseCircle.radius;
       const maxSubCircleDepth = baseCircle.radius - subCircleRadius;
-      const subCircleDepth = relativeDepth * maxSubCircleDepth;
+      const subCircleDepth =
+        relativeDepth === 0 || maxSubCircleDepth === 0
+          ? 0.00000001
+          : relativeDepth * maxSubCircleDepth;
       return {
-        radius: relativeRadius * baseCircle.radius,
+        radius:
+          subCircleRadius === baseCircle.radius
+            ? subCircleRadius - 0.00000001
+            : subCircleRadius,
         center: [
           subCircleDepth * Math.cos(relativePhase) + baseCircle.center[0],
           subCircleDepth * Math.sin(relativePhase) + baseCircle.center[1],
@@ -195,38 +326,19 @@ function getLoopNode(api: GetLoopNodeApi): LoopNode {
         pointA: baseCircle.center,
         pointB: subCircle.center,
       });
-      const subBaseMinBasePoint = getCirclePoint({
-        someCircle: baseCircle,
-        pointAngle: relativeSubCirclePhase,
+      const {
+        minIntersectionRadius,
+        intersectionRadiusMinMaxDelta,
+        maxIntersectionRadius,
+      } = getIntersectionRadiusData({
+        baseCircle,
+        subCircle,
+        relativeSubCirclePhase,
       });
-      const subBaseMinSubPoint = getCirclePoint({
-        someCircle: subCircle,
-        pointAngle: relativeSubCirclePhase,
-      });
-      const subBaseMinDistance = getDistanceBetweenPoints({
-        pointA: subBaseMinBasePoint,
-        pointB: subBaseMinSubPoint,
-      });
-      const minIntersectionRadius = subBaseMinDistance + subCircle.radius;
-      const subBaseMaxBasePoint = getCirclePoint({
-        someCircle: baseCircle,
-        pointAngle: relativeSubCirclePhase + Math.PI,
-      });
-      const subBaseMaxSubPoint = getCirclePoint({
-        someCircle: subCircle,
-        pointAngle: relativeSubCirclePhase + Math.PI,
-      });
-      const subBaseMaxDistance = getDistanceBetweenPoints({
-        pointA: subBaseMaxBasePoint,
-        pointB: subBaseMaxSubPoint,
-      });
-      const maxIntersectionRadius = subBaseMaxDistance + subCircle.radius;
-      const intersectionRadiusMinMaxDelta =
-        maxIntersectionRadius - minIntersectionRadius;
-      const intersectionCircleCount = 256;
+      const intersectionCircleCount = 512;
       const loopPoints = new Array(intersectionCircleCount - 2)
         .fill(null)
-        .reduce<Array<any>>(
+        .reduce<Array<LoopPoint>>(
           (loopPointsResult, _, circleIndex) => {
             const intersectionCircle = getIntersectionCircle({
               minIntersectionRadius,
@@ -278,7 +390,7 @@ function getLoopNode(api: GetLoopNodeApi): LoopNode {
           intersectionAngle: relativeSubCirclePhase + Math.PI,
         })
       );
-      loopPoints.sort((a, b) => a.outputAngle - b.outputAngle);
+      loopPoints.sort((a, b) => a[2] - b[2]);
       return loopPoints;
     },
   };
@@ -320,12 +432,63 @@ interface GetIntersectionBaseAngleApi {
 function getIntersectionBaseAngle(api: GetIntersectionBaseAngleApi): number {
   const { baseCircleRadius, baseSubCenterDistance, intersectionCircleRadius } =
     api;
-  return Math.acos(
-    (Math.pow(baseCircleRadius, 2) -
-      Math.pow(baseSubCenterDistance, 2) -
-      Math.pow(intersectionCircleRadius, 2)) /
-      (-2 * baseSubCenterDistance * intersectionCircleRadius)
+  const numerator =
+    Math.pow(baseCircleRadius, 2) -
+    Math.pow(baseSubCenterDistance, 2) -
+    Math.pow(intersectionCircleRadius, 2);
+  const denominator = -2 * baseSubCenterDistance * intersectionCircleRadius;
+  return (
+    Math.acos(numerator / denominator) ||
+    // weird js math precision workaround
+    Math.acos(
+      (numerator > 0 && denominator > 0) || (numerator < 0 && denominator < 0)
+        ? 1
+        : -1
+    )
   );
+}
+
+interface GetIntersectionRadiusDataApi {
+  baseCircle: Circle;
+  subCircle: Circle;
+  relativeSubCirclePhase: number;
+}
+
+function getIntersectionRadiusData(api: GetIntersectionRadiusDataApi) {
+  const { baseCircle, relativeSubCirclePhase, subCircle } = api;
+  const subBaseMinBasePoint = getCirclePoint({
+    someCircle: baseCircle,
+    pointAngle: relativeSubCirclePhase,
+  });
+  const subBaseMinSubPoint = getCirclePoint({
+    someCircle: subCircle,
+    pointAngle: relativeSubCirclePhase,
+  });
+  const subBaseMinDistance = getDistanceBetweenPoints({
+    pointA: subBaseMinBasePoint,
+    pointB: subBaseMinSubPoint,
+  });
+  const minIntersectionRadius = subBaseMinDistance + subCircle.radius;
+  const subBaseMaxBasePoint = getCirclePoint({
+    someCircle: baseCircle,
+    pointAngle: relativeSubCirclePhase + Math.PI,
+  });
+  const subBaseMaxSubPoint = getCirclePoint({
+    someCircle: subCircle,
+    pointAngle: relativeSubCirclePhase + Math.PI,
+  });
+  const subBaseMaxDistance = getDistanceBetweenPoints({
+    pointA: subBaseMaxBasePoint,
+    pointB: subBaseMaxSubPoint,
+  });
+  const maxIntersectionRadius = subBaseMaxDistance + subCircle.radius;
+  const intersectionRadiusMinMaxDelta =
+    maxIntersectionRadius - minIntersectionRadius;
+  return {
+    minIntersectionRadius,
+    maxIntersectionRadius,
+    intersectionRadiusMinMaxDelta,
+  };
 }
 
 interface GetIntersectionCircleApi {
@@ -348,6 +511,38 @@ function getIntersectionCircle(api: GetIntersectionCircleApi): Circle {
       relativeIntersectionRadius * intersectionRadiusMinMaxDelta +
       minIntersectionRadius,
   };
+}
+
+interface GetLoopTracePointApi {
+  baseCircle: Circle;
+  subCircle: Circle;
+  loopPoints: Array<LoopPoint>;
+  traceAngle: number;
+}
+
+function getLoopTracePoint(api: GetLoopTracePointApi) {
+  const { loopPoints, traceAngle, subCircle, baseCircle } = api;
+  const loopPointIndexA = loopPoints.findIndex((_, loopPointIndex) => {
+    if (loopPointIndex + 1 === loopPoints.length) return false;
+    const loopPointA = loopPoints[loopPointIndex];
+    const loopPointB = loopPoints[loopPointIndex + 1];
+    return traceAngle >= loopPointA[2] && traceAngle <= loopPointB[2];
+  });
+  const loopPointA = loopPoints[loopPointIndexA];
+  const loopPointB = loopPoints[(loopPointIndexA + 1) % loopPoints.length];
+  return getIntersectionPoint({
+    lineA: [
+      [loopPointA[0], loopPointA[1]],
+      [loopPointB[0], loopPointB[1]],
+    ],
+    lineB: [
+      subCircle.center,
+      [
+        baseCircle.radius * Math.cos(traceAngle) + subCircle.center[0],
+        baseCircle.radius * Math.sin(traceAngle) + subCircle.center[1],
+      ],
+    ],
+  });
 }
 
 interface GeometryNode<NodeType extends string, NodeEncoding, NodeGeometry> {

e520a0fb8d0d64a8ce41219726aeb3fad655ed33
jmath <clumsycomputer@gmail.com>
2022-10-10 16:54:33 -0600

    feature(LoopToyPage): moar progress on new page layout

--- components/pages/toys/loop/LoopToyPage.tsx
+++ components/pages/toys/loop/LoopToyPage.tsx
@@ -1,18 +1,73 @@
-import { useRef, useState } from "react";
+import { useEffect, useLayoutEffect, useMemo, useRef, useState } from "react";
 import { Page } from "../../../common/Page/Page";
-import { LoopStructure } from "./common/geometry/loop/encodings";
+import { Point2 } from "./common/geometry/general/encodings";
+import { LoopPoint, LoopStructure } from "./common/geometry/loop/encodings";
+import { getLoopPoint } from "./common/geometry/loop/getLoopPoint";
 import styles from "./LoopToyPage.module.scss";
 
 export function LoopToyPage() {
-  const focusedInputRef = useRef<HTMLInputElement>(null);
+  const activeCellInputRef = useRef<HTMLInputElement>(null);
   const [activeCellState, setActiveCellState] = useState<ActiveCellState>({
     stateType: "noActiveCell",
   });
+  useLayoutEffect(() => {
+    activeCellInputRef.current?.focus();
+  }, [activeCellState]);
   const [layersState, setLayersState] = useState<Array<LoopLayer>>([
     getNewLoopLayer({
       loopLayerId: generateLoopLayerId(),
     }),
   ]);
+  const loopPointsData = useMemo(() => {
+    const loopStructure = getLoopStructure({
+      someLoopLayers: layersState,
+    });
+    const pointCount = 1024;
+    const maxPointMagnitudeRef: [number] = [0];
+    const maxCosineMagnitudeRef: [number] = [0];
+    const maxSineMagnitudeRef: [number] = [0];
+    const loopPointsData: Array<
+      [LoopPoint, [Point2, [number]], [number, [number]], [number, [number]]]
+    > = [];
+    for (let pointIndex = 0; pointIndex < pointCount; pointIndex++) {
+      const loopPoint = getLoopPoint({
+        someLoopStructure: loopStructure,
+        inputAngle: ((2 * Math.PI) / pointCount) * pointIndex,
+      });
+      const deltaLoopPointX = loopPoint[0] - loopPoint[2][0];
+      const deltaLoopPointY = loopPoint[1] - loopPoint[2][1];
+      const loopPointMagnitude = Math.sqrt(
+        deltaLoopPointX * deltaLoopPointX + deltaLoopPointY * deltaLoopPointY
+      );
+      maxPointMagnitudeRef[0] =
+        loopPointMagnitude > maxPointMagnitudeRef[0]
+          ? loopPointMagnitude
+          : maxPointMagnitudeRef[0];
+      const zeroOriginLoopPoint: Point2 = [
+        loopPoint[0] - loopPoint[2][0],
+        loopPoint[1] - loopPoint[2][1],
+      ];
+      const pointCosine = loopPoint[0] - loopPoint[2][0];
+      const pointCosineMagnitude = Math.abs(pointCosine);
+      maxCosineMagnitudeRef[0] =
+        pointCosineMagnitude > maxCosineMagnitudeRef[0]
+          ? pointCosineMagnitude
+          : maxCosineMagnitudeRef[0];
+      const pointSine = loopPoint[1] - loopPoint[2][1];
+      const pointSineMagnitude = Math.abs(pointSine);
+      maxSineMagnitudeRef[0] =
+        pointSineMagnitude > maxSineMagnitudeRef[0]
+          ? pointSineMagnitude
+          : maxSineMagnitudeRef[0];
+      loopPointsData.push([
+        loopPoint,
+        [zeroOriginLoopPoint, maxPointMagnitudeRef],
+        [pointCosine, maxCosineMagnitudeRef],
+        [pointSine, maxSineMagnitudeRef],
+      ]);
+    }
+    return loopPointsData;
+  }, [layersState]);
   return (
     <Page
       pageContentContainerClassname={styles.pageContentContainer}
@@ -20,7 +75,74 @@ export function LoopToyPage() {
       pageTabTitle={"loop toy - clumsycomputer"}
       pageDescription={"a fun little toy for exploring loops"}
     >
-      <div style={{ display: "flex", flexDirection: "column", padding: 8 }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 8,
+          paddingBottom: 0,
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "row",
+          }}
+        >
+          <div style={{ flexGrow: 1, maxWidth: 256 + 128, display: "flex" }}>
+            <svg
+              viewBox={`${-1.25} ${-1.25} ${2.5} ${2.5}`}
+              // width={256}
+              // height={256}
+            >
+              <rect
+                x={-1.25}
+                y={-1.25}
+                width={2.5}
+                height={2.5}
+                fill={"gray"}
+              />
+              <g transform="scale(1,-1)">
+                <polygon
+                  points={loopPointsData
+                    .map(
+                      (someLoopPointData) =>
+                        `${
+                          someLoopPointData[1][0][0] /
+                          someLoopPointData[1][1][0]
+                        },${
+                          someLoopPointData[1][0][1] /
+                          someLoopPointData[1][1][0]
+                        }`
+                    )
+                    .join(" ")}
+                  fillOpacity={0}
+                  strokeWidth={0.03}
+                  stroke={"yellow"}
+                  strokeLinejoin={"round"}
+                  strokeLinecap={"round"}
+                />
+              </g>
+            </svg>
+          </div>
+        </div>
+        <div
+          style={{
+            marginTop: 8,
+            // borderTopStyle: "solid",
+            // borderTopColor: "lightgrey",
+            // borderTopWidth: 1,
+          }}
+        />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          padding: 8,
+          paddingBottom: 0,
+        }}
+      >
         {activeCellState.stateType === "noActiveCell" ? (
           <div
             style={{
@@ -42,6 +164,7 @@ export function LoopToyPage() {
             </div>
             <div style={{ display: "flex" }}>
               <input
+                ref={activeCellInputRef}
                 style={{ fontSize: 18, fontWeight: 600, fontStyle: "italic" }}
                 type={"number"}
                 step={0.04}
@@ -54,13 +177,38 @@ export function LoopToyPage() {
                     };
                   });
                 }}
+                onKeyDown={(someKeyDownEvent) => {
+                  if (someKeyDownEvent.key === "Enter") {
+                    setLayersState((currentLayersState) => {
+                      const nextLayersState = [...currentLayersState];
+                      nextLayersState.splice(activeCellState.layerIndex, 1, {
+                        ...nextLayersState[activeCellState.layerIndex],
+                        [activeCellState.fieldKey]:
+                          activeCellState.fieldValue === 1
+                            ? 1
+                            : activeCellState.fieldValue < 0
+                            ? ((activeCellState.fieldValue % 1) + 1) % 1
+                            : activeCellState.fieldValue > 1
+                            ? activeCellState.fieldValue % 1
+                            : activeCellState.fieldValue,
+                      });
+                      return nextLayersState;
+                    });
+                  }
+                }}
                 onBlur={() => {
                   setLayersState((currentLayersState) => {
                     const nextLayersState = [...currentLayersState];
                     nextLayersState.splice(activeCellState.layerIndex, 1, {
                       ...nextLayersState[activeCellState.layerIndex],
                       [activeCellState.fieldKey]:
-                        (activeCellState.fieldValue + 1) % 1,
+                        activeCellState.fieldValue === 1
+                          ? 1
+                          : activeCellState.fieldValue < 0
+                          ? ((activeCellState.fieldValue % 1) + 1) % 1
+                          : activeCellState.fieldValue > 1
+                          ? activeCellState.fieldValue % 1
+                          : activeCellState.fieldValue,
                     });
                     return nextLayersState;
                   });
@@ -70,7 +218,12 @@ export function LoopToyPage() {
           </div>
         )}
         <div
-          style={{ height: 1, backgroundColor: "lightgrey", marginTop: 12 }}
+          style={{
+            marginTop: 8,
+            borderTopStyle: "solid",
+            borderTopColor: "lightgrey",
+            borderTopWidth: 1,
+          }}
         />
       </div>
       <div
@@ -79,6 +232,7 @@ export function LoopToyPage() {
           display: "flex",
           flexDirection: "column",
           overflow: "scroll",
+          paddingTop: 8,
           paddingBottom: 8,
         }}
       >
@@ -272,7 +426,7 @@ function LoopLayerCell(props: LoopLayerCellProps) {
     >
       <button
         style={{
-          minWidth: 64,
+          width: 82,
           flexShrink: 1,
           overflow: "hidden",
           textOverflow: "ellipsis",
@@ -283,8 +437,81 @@ function LoopLayerCell(props: LoopLayerCellProps) {
         }}
         onClick={onClick}
       >
-        {cellValue.toFixed(4)}
+        {parseFloat(cellValue.toFixed(4))}
       </button>
     </div>
   );
+}
+
+interface GetLoopStructureApi {
+  someLoopLayers: Array<LoopLayer>;
+}
+
+function getLoopStructure(api: GetLoopStructureApi): LoopStructure {
+  const { someLoopLayers } = api;
+  const [currentLayer, ...remainingLoopLayers] = someLoopLayers;
+  return currentLayer
+    ? _getLoopStructure({
+        currentLayer,
+        remainingLoopLayers,
+      })
+    : {
+        structureType: "initial",
+        loopBase: {
+          radius: 1,
+          center: [0, 0],
+        },
+        subStructure: {
+          structureType: "terminal",
+          relativeSubRadius: 1,
+          relativeSubDepth: 0,
+          subPhase: 0,
+          subOrientation: 0,
+          loopRotation: 0,
+        },
+      };
+}
+
+interface _GetLoopStructureApi {
+  currentLayer: LoopLayer;
+  remainingLoopLayers: Array<LoopLayer>;
+}
+
+function _getLoopStructure(api: _GetLoopStructureApi): LoopStructure {
+  const { currentLayer, remainingLoopLayers } = api;
+  return remainingLoopLayers.length > 0
+    ? {
+        structureType: "initial",
+        loopBase: {
+          radius: 1,
+          center: [0, 0],
+        },
+        subStructure: {
+          structureType: "interposed",
+          relativeSubRadius: currentLayer.relativeSubRadius,
+          relativeSubDepth: currentLayer.relativeSubDepth,
+          subPhase: 2 * Math.PI * currentLayer.relativeSubPhase,
+          subOrientation: 2 * Math.PI * currentLayer.relativeSubOrientation,
+          loopRotation: 2 * Math.PI * currentLayer.relativeLoopRotation,
+          subStructure: _getLoopStructure({
+            currentLayer: remainingLoopLayers[0],
+            remainingLoopLayers: remainingLoopLayers.slice(1),
+          }).subStructure,
+        },
+      }
+    : {
+        structureType: "initial",
+        loopBase: {
+          radius: 1,
+          center: [0, 0],
+        },
+        subStructure: {
+          structureType: "terminal",
+          relativeSubRadius: currentLayer.relativeSubRadius,
+          relativeSubDepth: currentLayer.relativeSubDepth,
+          subPhase: 2 * Math.PI * currentLayer.relativeSubPhase,
+          subOrientation: 2 * Math.PI * currentLayer.relativeSubOrientation,
+          loopRotation: 2 * Math.PI * currentLayer.relativeLoopRotation,
+        },
+      };
 }

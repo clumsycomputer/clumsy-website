be4f4ed2bf7de3e04cc4e821f451d3586c8edc05
jmath <clumsycomputer@gmail.com>
2022-10-10 17:32:08 -0600

    fix(LoopToyPage): moar moar ios safari viewport attempts

--- components/pages/toys/loop/LoopToyPage.tsx
+++ components/pages/toys/loop/LoopToyPage.tsx
@@ -89,6 +89,7 @@ export function LoopToyPage() {
           display: "flex",
           flexDirection: "column",
           height: viewportHeight,
+          overflow: "scroll",
         }}
       >
         <div
